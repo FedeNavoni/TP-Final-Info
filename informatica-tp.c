e8a5883523a5df2f7e9c43a0301d97611d3f92f3
FedeNavoni <navonimerz@gmail.com>
2018-11-27 20:31:21 -0300

    Jere gato

--- informatica-tp.c
+++ informatica-tp.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 
-void CARGA(float array[][11],int *x)
+void CARGA(float array[][11])
 {
         FILE *arch;
         int i = 0;
@@ -16,16 +16,46 @@ void CARGA(float array[][11],int *x)
         }
 	fclose(arch);
 	printf("%d\n", i);
-	*x = i;
+}
+
+void SUMA_POLI(float array_1[][11],float array_2[],int x)
+{
+        float suma = 0;
+        for (int i = 0; i <= 10; i++)
+        {
+                for (int j = 0; j <= x; j++)
+                {
+                        suma += array_1[j][i];
+                }
+                array_2[i] = suma;
+                suma = 0;
+        }
 }
 
 
 void main()
 {
 	float polinomios[15][11];
+	float resultado[11];
 	int *cantpol;
 
-	CARGA(polinomios, cantpol);
+	CARGA(polinomios);
 
-	printf("%f %f ", polinomios[0][5], polinomios[2][5]);
+	for (int a = 0; a < 9; a++){
+                for (int j= 0; j <= 10; j++){
+                        if (polinomios[a][j] != 0){
+                                printf("%2.1fx^%d  ", polinomios[a][j], 10-j);
+			}
+		}
+		printf("\n");
+	}
+
+	SUMA_POLI(polinomios, resultado, 9);
+
+        for (int j= 0; j <= 10; j++){
+       		if (resultado[j] != 0){
+        		printf("%2.1fx^%d  ", resultado[j], 10-j);
+		}
+	}
+	printf("\n");
 }
